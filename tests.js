a3d0c5856349a8ffdb87aeddde738b994997515c
kewp <karlpenzhorn@gmail.com>
2022-04-10 10:30:13 +0300

    cleanup

--- tests.js
+++ tests.js
@@ -6,9 +6,9 @@ let tests = get_tests();
 if (tests.length==0) console.log('WARN no tests found');
 else
 {
-    let library = get_library();
+    let library = get_library(); // get latest auto source in dev/src
 
-    console.log('running tests');
+    console.log('running tests in tests/');
     
     tests.forEach(name => {
         const test = require('./tests/' + name + '.js');
