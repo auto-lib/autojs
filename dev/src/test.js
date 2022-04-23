46463a26f0cff5442fd99fde0ca7d03c05419232
kewp <karlpenzhorn@gmail.com>
2022-04-23 12:44:00 +0300

    comments

--- dev/src/test.js
+++ dev/src/test.js
@@ -1,37 +1,38 @@
 
+// i use a separate, numbered path for each version of the core library
 module.paths.push('./001_separate_boxes');
 
-let evts = [];
+// log everything that happens internally
+let evts = [], hook = (obj,v,fn,parm) => evts.push({ obj,v,fn,parm });
 
-let hook = (obj,v,fn,parm) => evts.push({ obj,v,fn,parm });
+// get wrap using hook function
+let { cache, error, pubsub } = require('trace')(hook, { cache: require('cache')() });
 
-let trace = require('trace');
-
-let { cache, error, pubsub } = trace(hook, {
-    cache: require('cache')(),
-    error: require('error')(),
-    pubsub: require('pubsub')()
-});
-
+// wrap main function with above internal objects
 let auto = obj => require('./001_separate_boxes/')(obj, { cache, error, pubsub });
 
+// object one
 let a = auto({
     x: 10,
     y: _ => _.x * 2 + _.w,
     log: _ => console.log('y =',_.y)
 });
 
+// object two (note: interdependent with 'a')
 let b = auto({
     w: 2,
     z: _ => _.x + _.y,
     log: _ => console.log('z =',_.z)
 });
 
+// set values (should react automatically)
 a.x = 5;
 b.w = 3;
 
+// show everything that happened internally
 console.log('evts',evts);
 
+// show all internal state variables
 console.log('state',{
     cache: cache(),
     error: error(),
