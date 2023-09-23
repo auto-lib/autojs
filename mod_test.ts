14ebac7ceb26635213de2bb3437461df6d0574d0
kewp <karlpenzhorn@gmail.com>
2023-09-23 14:23:25 +0530

    assert mod exports auto function

--- mod_test.ts
+++ mod_test.ts
@@ -1,8 +1,20 @@
-import { assertNotEquals } from "https://deno.land/std@0.202.0/assert/mod.ts";
-// import { auto } from "./mod.ts";
 
-Deno.test("mod.ts should exist", async () => {
+import { assertInstanceOf } from "https://deno.land/std@0.202.0/assert/assert_instance_of.ts";
+import { assertNotEquals } from "https://deno.land/std@0.202.0/assert/mod.ts";
+
+Deno.test("mod.ts should exist", async (t) => {
+
   const mod_ts = await Deno.readFile("./mod.ts").catch((e) => console.error(e));
   assertNotEquals(mod_ts, undefined);
+
+  await t.step("mod.ts should export auto function", async () => {
+
+    const mod = await import("./mod.ts");
+
+    assertNotEquals(mod.auto, undefined);
+    assertInstanceOf(mod.auto, Function);
+
+  })
+
 });
 
