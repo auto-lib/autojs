9637865f5624f1715190917a81e862a03fe8754d
kewp <karlpenzhorn@gmail.com>
2023-09-23 14:41:41 +0530

    cleanup suggested by copilot

--- mod_test.ts
+++ mod_test.ts
@@ -1,44 +1,27 @@
-
+import { assertExists } from "https://deno.land/std@0.202.0/assert/mod.ts";
 import { assertInstanceOf } from "https://deno.land/std@0.202.0/assert/assert_instance_of.ts";
-import { assertNotEquals } from "https://deno.land/std@0.202.0/assert/mod.ts";
 
-function get_latest_mod(root:string) {
-  let dirs: Deno.DirEntry[] = [];
-  for (const dirEntry of Deno.readDirSync(root)) dirs.push(dirEntry);
-  // sort
-  dirs.sort((a, b) => {
-    if (a.name < b.name) return -1;
-    else if (a.name > b.name) return 1;
-    else return 0;
-  });
-  // filter by .ts
-  dirs = dirs.filter((dirEntry) => dirEntry.name.endsWith(".ts"));
-
-  // get latest
-  return dirs[dirs.length - 1];
+function getLatestMod(root: string): Deno.DirEntry {
+  const dirs = [...Deno.readDirSync(root)]
+    .filter((dir) => dir.isFile && dir.name.endsWith(".ts"))
+    .sort((a, b) => b.name > a.name ? 1 : -1);
+  return dirs[0];
 }
 
 Deno.test("mod.ts should exist", async (t) => {
 
-  const root = './src';
-
-  const lastest = get_latest_mod(root).name;
-
-  const mod_path = `${root}/${lastest}`;
-
-  console.log("reading latest mod from:", mod_path);
-
-  const mod_ts = await Deno.readFile(mod_path).catch((e) => console.error(e));
-  assertNotEquals(mod_ts, undefined);
+  const root = "./src";
+  const latest = getLatestMod(root).name;
+  const modPath = `${root}/${latest}`;
+  console.log("reading latest mod from:", modPath);
+  const modTs = await Deno.readTextFile(modPath).catch(console.error);
+  assertExists(modTs);
 
   await t.step("mod.ts should export auto function", async () => {
-
-    const mod = await import("./mod.ts");
-
-    assertNotEquals(mod.auto, undefined);
+    const mod = await import(`${modPath}`);
+    assertExists(mod.auto);
     assertInstanceOf(mod.auto, Function);
-
-  })
-
+    const auto = mod.auto;
+    const _ = auto({});
+  });
 });
-
